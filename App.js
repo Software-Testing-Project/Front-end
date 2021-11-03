9bf131f2ca0844cc87ab8977da1b7f8551f42cc2
Ehsan <ehsanghazanfar2@gmail.com>
2021-11-03 18:23:22 +0500

    Camera Modal Working now but still not saving pictures

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
+
 import {
   StyleSheet,
   Text,
@@ -8,6 +9,7 @@ import {
   TouchableOpacity,
   TextInput,
   Button,
+  Modal,
 } from "react-native";
 import Camera_Module from "./Components/Camera";
 
@@ -27,6 +29,7 @@ export default function App() {
     },
   ]);
   const [temp, set_temp] = useState("");
+  const [opencam, setcam] = useState(false);
   const Press_Handler = (key) => {
     set_todos((prev_todos) => {
       return prev_todos.filter((todo) => todo.key != key);
@@ -44,31 +47,49 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View>
-        <Camera_Module></Camera_Module>
+        <Modal
+          visible={opencam}
+          animationType="slide"
+          presentationStyle="fullScreen"
+          style={styles.Modal}
+        >
+          <Camera_Module turnback={setcam}></Camera_Module>
+        </Modal>
       </View>
-      <View>
-        <Text style={styles.header}>TODO App</Text>
-      </View>
-      <View>
-        <TextInput
-          onChangeText={(e) => set_temp(e)}
-          placeholder="Enter new Todo"
-          style={styles.input}
+
+      <View style={styles.cam_btn}>
+        <Button
+          title="Open Camera"
+          onPress={() => {
+            setcam(true);
+          }}
         />
-        <View style={styles.Button}>
-          <Button title="Add" onPress={() => Add_todo()} />
+      </View>
+      <View style={styles.todo_APP}>
+        <View>
+          <Text style={styles.header}>TODO App</Text>
         </View>
-      </View>
-      <View style={styles.list}>
-        <FlatList
-          data={todos}
-          extraData={todos}
-          renderItem={({ item }) => (
-            <TouchableOpacity onPress={() => Press_Handler(item.key)}>
-              <Text style={styles.item}>Eat : {item.name}</Text>
-            </TouchableOpacity>
-          )}
-        />
+        <View>
+          <TextInput
+            onChangeText={(e) => set_temp(e)}
+            placeholder="Enter new Todo"
+            style={styles.input}
+          />
+          <View style={styles.Button}>
+            <Button title="Add" onPress={() => Add_todo()} />
+          </View>
+        </View>
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            extraData={todos}
+            renderItem={({ item }) => (
+              <TouchableOpacity onPress={() => Press_Handler(item.key)}>
+                <Text style={styles.item}>Eat : {item.name}</Text>
+              </TouchableOpacity>
+            )}
+          />
+        </View>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -86,7 +107,7 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 30,
     color: "red",
-    marginTop: "10%",
+    marginTop: 100,
     backgroundColor: "#5e4700",
     width: 300,
     textAlign: "center",
@@ -114,5 +135,11 @@ const styles = StyleSheet.create({
     width: 150,
     alignSelf: "center",
     margin: 20,
+  },
+  cam_btn: {
+    marginTop: 40,
+  },
+  todo_APP: {
+    flex: 1,
   },
 });
